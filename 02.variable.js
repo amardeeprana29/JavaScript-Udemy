9b62a206d6972acd92b1097e7b9f0d79814ab9a4
Amardeep Rana <amardeeprana2905@gmail.com>
2025-07-18 23:13:06 +0530

    add variable

---
+++ 02.variable.js
@@ -0,0 +1,36 @@
+// first way to declare a variable
+fullName = "Amardeep Rana";
+
+// 2nd way
+var countryName = "India";  // avvoid using "var"
+countryName ="USA"; // This will throw not error because countryName is a variable and can be reassigned.
+
+console.log(countryName);
+
+// 3rd way
+let cityName = "Mumbai";  // "let" use for changeable value
+
+// 4th way
+const webName = "My Portfolio";   // use "const" for fixed values
+//webName = "Demo project"; // This will throw an error because webName is a constant and cannot be reassigned.
+
+console.log(webName); 
+
+var isActive = true;
+console.log(isActive);
+
+var courseName;
+console.log(courseName); // This will log 'undefined' because courseName is declared but not initialized.
+
+
+
+// How to name variables
+//fullName     camel case
+//fullname
+//full_name    snake case
+//FullName     Pascal case
+//Full_Name
+
+console.log(fullName);
+console.log(countryName);
+console.log(cityName);
