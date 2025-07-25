98894dc6165f2ea13c8dfa003a18fe8295363cc3
Amardeep Rana <amardeeprana2905@gmail.com>
2025-07-25 08:53:05 +0530

    add

--- 02.variable.js
+++ 02.variable.js
@@ -1,40 +1,33 @@
+/* How to name variables
+fullName     camel case
+fullname
+full_name    snake case
+FullName     Pascal case
+Full_Name 
+*/
+
+
 // first way to declare a variable
 fullName = "Amardeep Rana";
+console.log(fullName);
 
 // 2nd way
 var countryName = "India";  // avvoid using "var"
-countryName ="USA"; // This will throw not error because countryName is a variable and can be reassigned.
+console.log(countryName);
 
+countryName ="USA"; // This will throw not error because countryName is a variable and can be reassigned.
 console.log(countryName);
 
 // 3rd way
 let cityName = "Mumbai";  // "let" use for changeable value
+console.log(cityName);
 
 // 4th way
 const webName = "My Portfolio";   // use "const" for fixed values
-//webName = "Demo project"; // This will throw an error because webName is a constant and cannot be reassigned.
-
 console.log(webName); 
 
-var isActive = true;
-console.log(isActive);
+webName = "Demo project"; // This will throw an error because webName is a constant and cannot be reassigned.
+console.log(webName);
 
-var courseName;
-console.log(courseName); // This will log 'undefined' because courseName is declared but not initialized.
 
-const number = 10;
-console.log(number);
 
-const percentage = 99.99;
-console.log(percentage);
-
-// How to name variables
-//fullName     camel case
-//fullname
-//full_name    snake case
-//FullName     Pascal case
-//Full_Name
-
-console.log(fullName);
-console.log(countryName);
-console.log(cityName);
--- 03.data_type.js
+++ 03.data_type.js
@@ -1,1 +1,12 @@
 let name = "Amardeep"; // String
+var isActive = true;
+console.log(isActive);
+
+var courseName;
+console.log(courseName); // This will log 'undefined' because courseName is declared but not initialized.
+
+const number = 10;
+console.log(number);
+
+const percentage = 99.99;
+console.log(percentage);
