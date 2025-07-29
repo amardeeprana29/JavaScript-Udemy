9c47e20f767291f403de966dafc23c1d937ca1ce
Amardeep Rana <amardeeprana2905@gmail.com>
2025-07-29 22:41:04 +0530

    Add Ternary operator

---
+++ 06. Condition-statement/4. Ternary-operator.js
@@ -0,0 +1,17 @@
+// Example 1
+let isLoggedIn = false;
+(isLoggedIn)? console.log("User is Logged In") : console.log("User is not Logged In");
+
+console.log("---------------------");
+
+//Example 2: Find Even or Odd Number
+
+let num = 5;
+
+(num%2 == 0) ? console.log("Even Number") : console.log("Odd Number");
+
+console.log("---------------------");
+
+// Example 3
+let marks = 40;
+let result = (marks >= 35) ? console.log("Pass") : console.log("Fail");
