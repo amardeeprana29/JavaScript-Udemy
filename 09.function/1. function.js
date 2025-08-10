7cd7ddc92fd4d3bfac5a980615dcc328694408b4
Amardeep Rana <amardeeprana2905@gmail.com>
2025-08-10 20:36:24 +0530

    add

--- 09.function/1. function.js
+++ 09.function/1. function.js
@@ -1,1 +1,10 @@
 // function is a block of code that performs a specific task and it is reusable
+// It’s like a machine — you give it input, it processes it, and returns output.
+
+// basic example
+
+function greet(){
+    console.log("Hello !");
+}
+
+greet();
