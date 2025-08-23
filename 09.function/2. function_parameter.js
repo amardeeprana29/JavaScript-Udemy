485b02c492526b577b4f0966cdec833e5b97e0cf
Amardeep Rana <amardeeprana2905@gmail.com>
2025-08-23 23:31:40 +0530

    add

--- 09.function/2. function_parameter.js
+++ 09.function/2. function_parameter.js
@@ -1,2 +1,8 @@
 // A parameter is like a placeholder variable inside a function.
 // When you call the function, you pass in arguments (real values) to replace those parameters.
+
+function greet(name){
+    console.log(name)
+}
+
+greet("Amardeep Rana");
